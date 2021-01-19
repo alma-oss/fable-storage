b10d0b61d54c03144e15540816275371a90224d3
Petr Chromec <petr.chromec@lmc.eu>
2021-01-19 17:23:16 +0100

    Fix generic functions

--- src/Lmc.Fable.Storage/LocalStorage.fs
+++ src/Lmc.Fable.Storage/LocalStorage.fs
@@ -4,21 +4,21 @@ namespace Lmc.Fable.Storage
 module LocalStorage =
     open Thoth.Json
 
-    let loadWith (decoder: Decoder<'T>) key: Result<'T,string> =
+    let inline loadWith (decoder: Decoder<'Data>) key: Result<'Data, string> =
         let o = Browser.WebStorage.localStorage.getItem key
         if isNull o then
             "No item found in local storage with key " + key |> Error
         else
             Decode.fromString decoder o
 
-    let load<'Data> key: Result<'Data, string> =
-        key |> loadWith Decoder.forData<'Data>
+    let inline load<'Data> key: Result<'Data, string> =
+        key |> loadWith (Decode.Auto.generateDecoder<'Data>())
 
     let delete key =
         Browser.WebStorage.localStorage.removeItem(key)
 
-    let saveWith (encode: 'Data -> string) key (data: 'Data) =
+    let inline saveWith (encode: 'Data -> string) key (data: 'Data) =
         Browser.WebStorage.localStorage.setItem(key, encode data)
 
-    let save key (data: 'Data) =
+    let inline save key (data: 'Data) =
         data |> saveWith (fun data -> Encode.Auto.toString(0, data)) key
